0579e4f8e2ad6a17ab7202ef32df118d8ffaf0c9
jorgeduje <jorgeduje4@gmail.com>
2023-07-31 13:45:35 -0300

    se agrega maquetacion del login

--- src/components/pages/login/Login.jsx
+++ src/components/pages/login/Login.jsx
@@ -1,10 +1,140 @@
+import {
+  Box,
+  Button,
+  FormControl,
+  Grid,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import GoogleIcon from "@mui/icons-material/Google";
+
+import { useNavigate } from "react-router-dom";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>
-        <h1>Login</h1>
-    </div>
-  )
-}
+  const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
 
-export default Login
+  const handleClickShowPassword = () => setShowPassword(!showPassword);
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        // backgroundColor: theme.palette.secondary.main,
+      }}
+    >
+      <form action="">
+        <Grid
+          container
+          rowSpacing={2}
+          // alignItems="center"
+          justifyContent={"center"}
+        >
+          <Grid item xs={10} md={12}>
+            <TextField name="email" label="Email" fullWidth />
+          </Grid>
+          <Grid item xs={10} md={12}>
+            <FormControl variant="outlined" fullWidth>
+              <InputLabel htmlFor="outlined-adornment-password">
+                Password
+              </InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                type={showPassword ? "text" : "password"}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      edge="end"
+                    >
+                      {showPassword ? (
+                        <VisibilityOff color="primary" />
+                      ) : (
+                        <Visibility color="primary" />
+                      )}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+              />
+            </FormControl>
+          </Grid>
+          <Grid container justifyContent="center" spacing={3} mt={2}>
+            <Grid item xs={8} md={5}>
+              <Button
+                variant="contained"
+                fullWidth
+                type="submit"
+                sx={{
+                  color: "white",
+                  textTransform: "none",
+                  textShadow: "2px 2px 2px grey",
+                }}
+              >
+                Ingresar
+              </Button>
+            </Grid>
+            <Grid item xs={8} md={5}>
+              <Tooltip title="ingresa con google">
+                <Button
+                  variant="contained"
+                  startIcon={<GoogleIcon />}
+                  // onClick={handleLoginGoggle}
+                  fullWidth
+                  sx={{
+                    color: "white",
+                    textTransform: "none",
+                    textShadow: "2px 2px 2px grey",
+                  }}
+                >
+                  Ingresa con google
+                </Button>
+              </Tooltip>
+            </Grid>
+            <Grid item xs={8} md={8}>
+              <Typography
+                color={"secondary.primary"}
+                variant={"h6"}
+                mt={1}
+                align="center"
+              >
+                ¿Aun no tienes cuenta?
+              </Typography>
+            </Grid>
+            <Grid item xs={8} md={5}>
+              <Tooltip title="ingresa con google">
+                <Button
+                  variant="contained"
+                  fullWidth
+                  onClick={() => navigate("/register")}
+                  type="button"
+                  sx={{
+                    color: "white",
+                    textTransform: "none",
+                    textShadow: "2px 2px 2px grey",
+                  }}
+                >
+                  Registrate
+                </Button>
+              </Tooltip>
+            </Grid>
+          </Grid>
+        </Grid>
+      </form>
+    </Box>
+  );
+};
+
+export default Login;
